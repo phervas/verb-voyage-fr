0b85d5c36e54d06c4503d0adb0f3ddf2e24e4278
Patricio Hervas <patricio@dev.com>
2025-10-31 13:40:23 +0100

    Added new verbs and examples

--- src/data/verbs.ts
+++ src/data/verbs.ts
@@ -1,12 +1,520 @@
 import { IrregularVerb } from "@/types/game";
 
 export const IRREGULAR_VERBS: IrregularVerb[] = [
+  { infinitive: "be", past: "was / were", pastParticiple: "been", french: "être" },
+  { infinitive: "beat", past: "beat", pastParticiple: "beaten", french: "battre" },
+  { infinitive: "become", past: "became", pastParticiple: "become", french: "devenir" },
+  { infinitive: "begin", past: "began", pastParticiple: "begun", french: "commencer" },
+  { infinitive: "bet", past: "bet", pastParticiple: "bet", french: "parier" },
+  { infinitive: "bind", past: "bound", pastParticiple: "bound", french: "lier / relier" },
+  { infinitive: "bite", past: "bit", pastParticiple: "bitten", french: "mordre" },
+  { infinitive: "bleed", past: "bled", pastParticiple: "bled", french: "saigner" },
+  { infinitive: "blow", past: "blew", pastParticiple: "blown", french: "souffler / gonfler" },
+  { infinitive: "break", past: "broke", pastParticiple: "broken", french: "casser" },
+  { infinitive: "bring", past: "brought", pastParticiple: "brought", french: "apporter" },
+  { infinitive: "build", past: "built", pastParticiple: "built", french: "construire" },
+  { infinitive: "burn", past: "burnt / burned", pastParticiple: "burnt / burned", french: "brûler" },
+  { infinitive: "burst", past: "burst", pastParticiple: "burst", french: "éclater" },
+  { infinitive: "buy", past: "bought", pastParticiple: "bought", french: "acheter" },
+  { infinitive: "can", past: "could", pastParticiple: "could", french: "pouvoir" },
+  { infinitive: "catch", past: "caught", pastParticiple: "caught", french: "attraper" },
+  { infinitive: "choose", past: "chose", pastParticiple: "chosen", french: "choisir" },
+  { infinitive: "come", past: "came", pastParticiple: "come", french: "venir" },
+  { infinitive: "cost", past: "cost", pastParticiple: "cost", french: "coûter" },
+  { infinitive: "cut", past: "cut", pastParticiple: "cut", french: "couper" },
+  { infinitive: "deal", past: "dealt", pastParticiple: "dealt", french: "distribuer" },
+  { infinitive: "do", past: "did", pastParticiple: "done", french: "faire" },
+  { infinitive: "draw", past: "drew", pastParticiple: "drawn", french: "dessiner / tirer" },
+  { infinitive: "dream", past: "dreamt / dreamed", pastParticiple: "dreamt / dreamed", french: "rêver" },
+  { infinitive: "drink", past: "drank", pastParticiple: "drunk", french: "boire" },
+  { infinitive: "drive", past: "drove", pastParticiple: "driven", french: "conduire" },
+  { infinitive: "eat", past: "ate", pastParticiple: "eaten", french: "manger" },
+  { infinitive: "fall", past: "fell", pastParticiple: "fallen", french: "tomber" },
+  { infinitive: "feed", past: "fed", pastParticiple: "fed", french: "nourrir" },
+  { infinitive: "feel", past: "felt", pastParticiple: "felt", french: "se sentir / ressentir" },
+  { infinitive: "fight", past: "fought", pastParticiple: "fought", french: "se battre" },
+  { infinitive: "find", past: "found", pastParticiple: "found", french: "trouver" },
+  { infinitive: "fly", past: "flew", pastParticiple: "flown", french: "voler (dans les airs)" },
+  { infinitive: "forbid", past: "forbade", pastParticiple: "forbidden", french: "interdire" },
+  { infinitive: "forget", past: "forgot", pastParticiple: "forgotten / forgot", french: "oublier" },
+  { infinitive: "forgive", past: "forgave", pastParticiple: "forgiven", french: "pardonner" },
+  { infinitive: "freeze", past: "froze", pastParticiple: "frozen", french: "geler" },
+  { infinitive: "get", past: "got", pastParticiple: "gotten / got", french: "obtenir" },
+  { infinitive: "give", past: "gave", pastParticiple: "given", french: "donner" },
+  { infinitive: "go", past: "went", pastParticiple: "gone", french: "aller" },
+  { infinitive: "grow", past: "grew", pastParticiple: "grown", french: "grandir / pousser" },
+  { infinitive: "hang", past: "hung", pastParticiple: "hung", french: "tenir / pendre" },
+  { infinitive: "have", past: "had", pastParticiple: "had", french: "avoir" },
+  { infinitive: "hear", past: "heard", pastParticiple: "heard", french: "entendre" },
+  { infinitive: "hide", past: "hid", pastParticiple: "hidden", french: "cacher" },
+  { infinitive: "hit", past: "hit", pastParticiple: "hit", french: "taper / appuyer" },
+  { infinitive: "hold", past: "held", pastParticiple: "held", french: "tenir" },
+  { infinitive: "hurt", past: "hurt", pastParticiple: "hurt", french: "blesser" },
+  { infinitive: "keep", past: "kept", pastParticiple: "kept", french: "garder" },
+  { infinitive: "know", past: "knew", pastParticiple: "known", french: "connaître / savoir" },
+  { infinitive: "lead", past: "led", pastParticiple: "led", french: "mener / guider" },
+  { infinitive: "learn", past: "learnt", pastParticiple: "learnt", french: "apprendre" },
+  { infinitive: "leave", past: "left", pastParticiple: "left", french: "laisser / quitter / partir" },
+  { infinitive: "lend", past: "lent", pastParticiple: "lent", french: "prêter" },
+  { infinitive: "let", past: "let", pastParticiple: "let", french: "permettre / louer" },
+  { infinitive: "light", past: "lit / lighted", pastParticiple: "lit / lighted", french: "allumer" },
+  { infinitive: "lose", past: "lost", pastParticiple: "lost", french: "perdre" },
+  { infinitive: "make", past: "made", pastParticiple: "made", french: "fabriquer" },
+  { infinitive: "mean", past: "meant", pastParticiple: "meant", french: "signifier" },
+  { infinitive: "meet", past: "met", pastParticiple: "met", french: "rencontrer" },
+  { infinitive: "pay", past: "paid", pastParticiple: "paid", french: "payer" },
+  { infinitive: "put", past: "put", pastParticiple: "put", french: "mettre" },
+  { infinitive: "read", past: "read", pastParticiple: "read", french: "lire" },
+  { infinitive: "ride", past: "rode", pastParticiple: "ridden", french: "monter (à vélo)" },
+  { infinitive: "ring", past: "rang", pastParticiple: "rung", french: "sonner / téléphoner" },
+  { infinitive: "rise", past: "rose", pastParticiple: "risen", french: "lever" },
+  { infinitive: "run", past: "ran", pastParticiple: "run", french: "courir" },
+  { infinitive: "saw", past: "saw / sawed", pastParticiple: "sawn / sawed", french: "scier" },
+  { infinitive: "say", past: "said", pastParticiple: "said", french: "dire" },
+  { infinitive: "see", past: "saw", pastParticiple: "seen", french: "voir" },
+  { infinitive: "sell", past: "sold", pastParticiple: "sold", french: "vendre" },
+  { infinitive: "send", past: "sent", pastParticiple: "sent", french: "envoyer" },
+  { infinitive: "shake", past: "shook", pastParticiple: "shaken", french: "secouer" },
+  { infinitive: "shine", past: "shone", pastParticiple: "shone", french: "briller" },
+  { infinitive: "shoot", past: "shot", pastParticiple: "shot", french: "tirer / fusiller" },
+  { infinitive: "show", past: "showed", pastParticiple: "shown", french: "montrer" },
+  { infinitive: "shut", past: "shut", pastParticiple: "shut", french: "fermer" },
+  { infinitive: "sing", past: "sang", pastParticiple: "sung", french: "chanter" },
+  { infinitive: "sit", past: "sat", pastParticiple: "sat", french: "s'asseoir" },
+  { infinitive: "sleep", past: "slept", pastParticiple: "slept", french: "dormir" },
+  { infinitive: "smell", past: "smelt", pastParticiple: "smelt", french: "sentir" },
+  { infinitive: "speak", past: "spoke", pastParticiple: "spoken", french: "parler" },
+  { infinitive: "spell", past: "spelt", pastParticiple: "spelt", french: "épeler" },
+  { infinitive: "spend", past: "spent", pastParticiple: "spent", french: "dépenser / passer" },
+  { infinitive: "stand", past: "stood", pastParticiple: "stood", french: "être debout" },
+  { infinitive: "steal", past: "stole", pastParticiple: "stolen", french: "dérober / voler" },
+  { infinitive: "strike", past: "struck", pastParticiple: "stricken / struck", french: "frapper" },
+  { infinitive: "swim", past: "swam", pastParticiple: "swum", french: "nager" },
+  { infinitive: "take", past: "took", pastParticiple: "taken", french: "prendre" },
+  { infinitive: "teach", past: "taught", pastParticiple: "taught", french: "enseigner" },
+  { infinitive: "tear", past: "tore", pastParticiple: "torn", french: "déchirer" },
+  { infinitive: "tell", past: "told", pastParticiple: "told", french: "dire / raconter" },
+  { infinitive: "think", past: "thought", pastParticiple: "thought", french: "penser" },
+  { infinitive: "throw", past: "threw", pastParticiple: "thrown", french: "jeter" },
+  { infinitive: "understand", past: "understood", pastParticiple: "understood", french: "comprendre" },
+  { infinitive: "wake", past: "woke", pastParticiple: "woken", french: "réveiller" },
+  { infinitive: "wear", past: "wore", pastParticiple: "worn", french: "porter (un vêtement)" },
+  { infinitive: "win", past: "won", pastParticiple: "won", french: "gagner" },
+  { infinitive: "write", past: "wrote", pastParticiple: "written", french: "écrire" }
+];
+
+export const SAMPLE_SENTENCES = {
+  "be": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "beat": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "become": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "begin": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "bet": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "bind": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "bite": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "bleed": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "blow": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "break": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "bring": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "build": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "burn": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "burst": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "buy": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "can": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "catch": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "choose": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "come": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "cost": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "cut": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "deal": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "do": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "draw": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "dream": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "drink": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "drive": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "eat": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "fall": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "feed": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "feel": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "fight": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "find": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "fly": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "forbid": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "forget": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "forgive": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "freeze": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "get": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "give": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "go": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "grow": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "hang": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "have": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "hear": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "hide": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "hit": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "hold": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "hurt": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "keep": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "know": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "lead": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "learn": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "leave": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "lend": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "let": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "light": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "lose": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "make": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "mean": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "meet": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "pay": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "put": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "read": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "ride": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "ring": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "rise": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "run": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "saw": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "say": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "see": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "sell": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "send": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "shake": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "shine": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "shoot": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "show": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "shut": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "sing": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "sit": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "sleep": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "smell": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "speak": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "spell": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "spend": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "stand": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "steal": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "strike": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "swim": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "take": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "teach": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "tear": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "tell": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "think": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "throw": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "understand": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "wake": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "wear": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "win": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ],
+  "write": [
+    "She _____ yesterday.",
+    "They have _____ already."
+  ]
+};
+
+
+/*
+
   { infinitive: "leave", past: "left", pastParticiple: "left", french: "quitter, partir" },
   {
     infinitive: "lend",
     past: "lent",
     pastParticiple: "lent",
-    french: "prêter (donner un coup de main / prêter une oreille attentive)",
+    french: "prêter (donner un coup de main / prêter une oreille attentive)"
   },
   { infinitive: "let", past: "let", pastParticiple: "let", french: "permettre ; laisser" },
   { infinitive: "let down", past: "let down", pastParticiple: "let down", french: "laisser tomber (décevoir)" },
@@ -46,7 +554,7 @@ export const IRREGULAR_VERBS: IrregularVerb[] = [
     infinitive: "strike",
     past: "struck",
     pastParticiple: "struck",
-    french: "frapper, donner un coup ; passer un accord",
+    french: "frapper, donner un coup ; passer un accord"
   },
   { infinitive: "swear", past: "swore", pastParticiple: "sworn", french: "jurer" },
   { infinitive: "swim", past: "swam", pastParticiple: "swum", french: "nager" },
@@ -57,9 +565,7 @@ export const IRREGULAR_VERBS: IrregularVerb[] = [
   { infinitive: "think", past: "thought", pastParticiple: "thought", french: "penser" },
   { infinitive: "throw", past: "threw", pastParticiple: "thrown", french: "jeter, lancer" },
   { infinitive: "understand", past: "understood", pastParticiple: "understood", french: "comprendre" },
-];
 
-export const SAMPLE_SENTENCES = {
   leave: ["She _____ the house at 8 o’clock.", "They have _____ for Paris already."],
   lend: ["He _____ me his pen yesterday.", "She has _____ her friend some money."],
   let: ["Dad _____ us play outside.", "They have _____ him join the game."],
@@ -106,9 +612,7 @@ export const SAMPLE_SENTENCES = {
   think: ["I _____ about the answer.", "He has _____ of a great idea."],
   throw: ["She _____ the ball to me.", "We have _____ the rubbish away."],
   understand: ["I finally _____ the rule.", "She has _____ the homework."],
-};
 
-/*
 export const IRREGULAR_VERBS: IrregularVerb[] = [
   {
     infinitive: 'set',
